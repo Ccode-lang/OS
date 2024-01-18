03906a289b85fd66b5c8f84786922a80c09604f2
Ccode-lang <coop112097@gmail.com>
2024-01-18 16:29:43 -0700

    Ports

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 nasm boot/kernel_entry.asm -f elf -o kernel_entry.o
 i686-linux-gnu-gcc -fno-pie -ffreestanding -c kernel/kernel.c -o kernel.o
+i686-linux-gnu-gcc -fno-pie -ffreestanding -c drivers/ports.c -o ports.o
 
-i686-linux-gnu-ld -o kernel.bin -Ttext 0x1000 kernel_entry.o kernel.o --oformat binary
+i686-linux-gnu-ld -o kernel.bin -Ttext 0x1000 kernel_entry.o kernel.o ports.o --oformat binary
 
 
 nasm boot/boot.asm -f bin -o boot.bin
