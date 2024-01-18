8ba67e0587beff3cbc78900f434e232658ba5f52
Ccode-lang <coop112097@gmail.com>
2024-01-17 21:25:24 -0700

    Build files

---
+++ build.sh
@@ -0,0 +1,10 @@
+nasm kernel_entry.asm -f elf -o kernel_entry.o
+i686-linux-gnu-gcc -fno-pie -ffreestanding -c kernel.c -o kernel.o
+
+i686-linux-gnu-ld -o kernel.bin -Ttext 0x1000 kernel_entry.o kernel.o --oformat binary
+
+
+nasm boot.asm -f bin -o boot.bin
+
+
+cat boot.bin kernel.bin > os-image.bin
