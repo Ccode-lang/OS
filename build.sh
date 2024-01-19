4186c334aa206cfd67e0e99be33f28c5ced3a413
Ccode-lang <coop112097@gmail.com>
2024-01-19 10:59:56 -0700

    scrolling

--- build.sh
+++ build.sh
@@ -1,13 +1,14 @@
 # Compile kernel objects
 nasm boot/kernel_entry.asm -f elf -o kernel_entry.o
 i686-linux-gnu-gcc -fno-pie -ffreestanding -c kernel/kernel.c -o kernel.o
+i686-linux-gnu-gcc -fno-pie -ffreestanding -c kernel/util.c -o util.o
 
 # Compile drivers
 i686-linux-gnu-gcc -fno-pie -ffreestanding -c drivers/ports.c -o ports.o
 i686-linux-gnu-gcc -fno-pie -ffreestanding -c drivers/screen.c -o screen.o
 
 # Link kernel and drivers
-i686-linux-gnu-ld -o kernel.bin -Ttext 0x1000 kernel_entry.o kernel.o ports.o screen.o --oformat binary
+i686-linux-gnu-ld -o kernel.bin -Ttext 0x1000 kernel_entry.o kernel.o util.o ports.o screen.o --oformat binary
 
 # Compile boot sector
 nasm boot/boot.asm -f bin -o boot.bin
